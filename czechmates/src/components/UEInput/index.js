529a63a58498c30766174230039c7534b700dd0c
Zara-Masino <masinoz@merrimack.edu>
2023-10-25 23:30:46 -0400

    commenting and cleaning

--- czechmates/src/components/UEInput/index.js
+++ czechmates/src/components/UEInput/index.js
@@ -1,9 +1,11 @@
 import Form from 'react-bootstrap/Form';
 
+// component to display an input that is not editable
+// given the value to display
 function UEInput({ value }) {
-
     return (
         <>
+            {/* set the value and disable the form input */}
             <Form.Control
                 value={value}
                 disabled="true"
--- czechmates/src/components/World/index.js
+++ czechmates/src/components/World/index.js
@@ -3,19 +3,20 @@ import ConfirmationPopup from "../ConfirmationPopup";
 import ViewWorldPopup from '../ViewWorldPopup';
 import ManageWorldPopup from '../ManageWorldPopup';
 
+// a component to display the worlds with their button options
 const World = () => {
     return (
         <div>
-            <div style={{backgroundColor: "lightblue"}}>
+            <div style={{ backgroundColor: "lightblue" }}>
                 World Name
-                {/* check if they own the world
+                {/* future: check if they own the world
                 if they do then add a star
                 set the button names accordingly */}
-                <ManageWorldPopup title="World Name" button={"Manage"}/>
-                <ViewWorldPopup name={"World Name"}/>
-                <ConfirmationPopup name={"Remove/Leave"}/>
+                {/* modals for each of the given actions: manage, view, remove/leave */}
+                <ManageWorldPopup title="World Name" button={"Manage"} />
+                <ViewWorldPopup name={"World Name"} />
+                <ConfirmationPopup name={"Remove/Leave"} />
             </div>
-
         </div>
     );
 };
