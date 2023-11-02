d16e4c30bbd4679b335f1a57e9a920f328489a5f
Zara-Masino <masinoz@merrimack.edu>
2023-11-02 19:07:49 -0400

    added read/write fns for db, changed json AGAIN

--- czechmates/src/App.js
+++ czechmates/src/App.js
@@ -20,42 +20,80 @@ import Func from './utils/attributeFunctions';
 import { ref, set } from "firebase/database";
 import { db } from './firebase';
 import { onValue } from "firebase/database";
-import {child, get } from "firebase/database";
+import { child, get } from "firebase/database";
 
 // anything in this app script will appear/be available on every page
 function App() {
 
-   // https://firebase.google.com/docs/database/web/read-and-write
-    function writeUserData(userId, name, email, imageUrl) {
+    // https://firebase.google.com/docs/database/web/read-and-write
+    function writeFakeUserData(userId, name, email, imageUrl) {
         set(ref(db, 'users/' + userId), {
             username: name,
             email: email,
             profile_picture: imageUrl
         });
     }
+    // writeFakeUserData("1", "2", "3", "4");
 
-    // writeUserData("1", "2", "3", "4");
 
-    // const starCountRef = ref(db, 'users/');
-    // console.log(starCountRef);
-    // onValue(starCountRef, (snapshot) => {
-    //     console.log("in");
-    //     const data = snapshot.val();
-    //     console.log(data);
-    //     // updateStarCount(postElement, data);
-    // });
+    function writeUserData(userId, adminStatus, email, followers, following, friends, mode, name) {
+        set(ref(db, 'Users/' + userId), {
+            Admin_Status: adminStatus,
+            Email: email,
+            Followers: followers,
+            Following: following,
+            Friends: friends,
+            Light_Mode: mode,
+            Name: name
+        });
+    }
+
+    // writeUserData("User3", "False", "user3@gmail.com", ["F1"], ["F2"], ["F3"], "Dark", "User3");
+
+
+    function writeWorldData(userId, inviteCode, members, name, schedule) {
+        set(ref(db, 'Worlds/' + userId + "/Worlds"),
+            {
+                Invite_Code: inviteCode,
+                Members: members,
+                Name: name,
+                Schedule: schedule
+            });
+    }
+
+    // writeWorldData("User3", "code4", ["M1", "M2"], "world2 (3)", "Sundays at 5");
+
+
+    // I got lazy and only did the minimum in general but the rest should be the same
+    function writeCharacterData(userID, charId, name, concept) {
+        set(ref(db, 'Characters/' + userID + "/" + charId + "/General"),
+            {
+                Name: name,
+                High_Concept: concept
+            });
+    }
+
+    // writeCharacterData("User3", "CharID1", "Char1", "rogue");
+
+
 
     // https://firebase.google.com/docs/database/web/read-and-write
-    const dbRef = ref(db);
-    get(child(dbRef, `users/`)).then((snapshot) => {
-        if (snapshot.exists()) {
-            console.log(snapshot.val());
-        } else {
-            console.log("No data available");
-        }
-    }).catch((error) => {
-        console.error(error);
-    });
+    // read data
+    function readUserData() {
+        const dbRef = ref(db);
+        get(child(dbRef, `Codes`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                console.log(snapshot.val());
+            } else {
+                console.log("No data available");
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
+    // readUserData();
+
 
 
 
