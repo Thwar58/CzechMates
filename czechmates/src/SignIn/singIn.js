e297aaa3164e2d5b8e771aa32b001443874418c3
camchrissis <camchrissis@gmail.com>
2023-11-07 20:13:31 -0500

    OAuth functional, recieving confirmation from Firebase side.

--- czechmates/src/SignIn/singIn.js
+++ czechmates/src/SignIn/singIn.js
@@ -3,7 +3,7 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from './firebaseConfig'; 
 import Popup from "../components/Modal";
 
-export default function Home() {
+export default function HomePage() {
     const handleGoogle = (e) => {
         const provider =  new GoogleAuthProvider();
         return signInWithPopup(auth, provider)
--- czechmates/src/pages/index.js
+++ czechmates/src/pages/index.js
@@ -8,11 +8,21 @@ import PopoverPositions from '../components/Reference/Popover';
 import Spacer from '../components/Reference/Spacer';
 import Popup from '../components/Reference/Modal';
 import DropDownShowsValue from '../components/DropDownShowsValue';
+import Button from 'react-bootstrap/Button';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../SignIn/firebaseConfig';
 
 // this is the home page, as of now it is mostly reference material and will be largely removed
 const Home = () => {
+
+    const handleGoogle = (e) => {
+        const provider =  new GoogleAuthProvider();
+        return signInWithPopup(auth, provider)
+
+    }
     return (
         <>
+        <Button onClick={handleGoogle}>Sign in with Google </Button>
             <div>
                 <h1>This is the Home page</h1>
             </div>
